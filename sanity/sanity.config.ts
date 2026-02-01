992750b7da296494e7f384b3587b5a885836a5a0
Fachrizal Rifahdi <fachrizalrifahdi@gmail.com>
2026-02-01 11:50:26 +0800

    Update Sanity Studio with correct project ID

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
+
+export default defineConfig({
+  projectId: 'illlgd5e',
+  dataset: 'production',
+  plugins: [
+    structureTool({
+      structure: (S) => S.list().title('Content').items([
+        S.documentTypeListItem('project').title('Projects'),
+      ]),
+    }),
+    visionTool(),
+  ],
+  schema: {
+    types: schemaTypes,
+  },
+})
---
+++ studio/static/sanity-BMqrZ9Ya.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-BD0Y-UDa.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
